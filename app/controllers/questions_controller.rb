a086bda2fb1104d868c2189ae86fe553fe444026
Alexey <alexx10@mail.ru>
2022-03-22 20:32:28 +0300

    Write action and view for show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,21 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: :show
+
   def index
     @questions = Question.all
   end
 
+  def show
+
+  end
+
   def new
     @question = Question.new
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
