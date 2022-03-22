1009dc0f0357ffc2ae200ba02d9ebedac9d746ad
Alexey <alexx10@mail.ru>
2022-03-22 21:43:54 +0300

    Write test for Answer create action with valid attributes

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
   describe "GET #new" do
-    let(:question) { create(:question) }
     before { get :new, params: { question_id: question.id } }
 
     it "assigns the question" do
@@ -15,6 +16,23 @@ RSpec.describe AnswersController, type: :controller do
 
     it "renders a new view" do
       expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid data" do
+      it 'saves a new answer in the database' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to question show view' do
+        post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
+        expect(response).to redirect_to question_path assigns(:answer).question_id
+      end
+    end
+
+    context "with invalid data" do
+
     end
   end
 
