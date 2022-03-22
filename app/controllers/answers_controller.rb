a69bbc352a685a08298082198683c82849d0f6c1
Alexey <alexx10@mail.ru>
2022-03-22 21:22:28 +0300

    Write Answer new action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: :new
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  private
+
+  def set_question
+    @question = Question.find_by(id: params[:question_id])
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   describe "GET #new" do
     let(:question) { create(:question) }
-    before { get :new, params: { question: question } }
+    before { get :new, params: { question_id: question.id } }
 
     it "assigns the question" do
-      expect(assigns(:question)).to eq :question
+      expect(assigns(:question)).to eq question
     end
 
     it "assigns new Answer to @answer" do
