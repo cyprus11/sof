29d5a36c409a416ed257427f8a2e521a8f28d894
Alexey <alexx10@mail.ru>
2022-03-22 20:51:27 +0300

    Write test for Answer new action

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  describe "GET #new" do
+    let(:question) { create(:question) }
+    before { get :new, params: { question: question } }
+
+    it "assigns the question" do
+      expect(assigns(:question)).to eq :question
+    end
+
+    it "assigns new Answer to @answer" do
+      expect(assigns(:answer)).to be_a_new Answer
+    end
+
+    it "renders a new view" do
+      expect(response).to render_template :new
+    end
+  end
 
 end
