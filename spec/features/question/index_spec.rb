174f260651a20354d94ca1360d2e3a720f3aea82
Alexey <alexx10@mail.ru>
2022-04-01 09:48:23 +0300

    Feature test question index

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'User can see all questions', %q{
+  probably he can find answer on his question
+} do
+  let!(:question) { create(:question) }
+
+  scenario "User open site and see questions" do
+    visit root_path
+    expect(page).to have_content 'Questions'
+    expect(page).to have_content question.title
+  end
+end
