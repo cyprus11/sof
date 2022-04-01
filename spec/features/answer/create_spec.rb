9e0bed507cff9aa1af11db04f0707b8f155d19f3
Alexey <alexx10@mail.ru>
2022-04-01 12:03:05 +0300

    Feature test create answer

---
+++ spec/features/answer/create_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can create answer', %q{
+  get an answer on a question
+} do
+  given(:question) { create(:question) }
+
+  scenario 'User visit page with question and print answer' do
+    visit question_path(question)
+    fill_in 'Answer', with: 'answer on question'
+    click_on 'Publish'
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content 'answer on question'
+  end
+end
