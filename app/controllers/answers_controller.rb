11e017a610f5eaa954f9f7faa44140a5afd96d63
Alexey <alexx10@mail.ru>
2022-04-04 21:26:10 +0300

    Test for delete answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   before_action :set_question, only: %i[create]
+  before_action :find_answer, only: :destroy
 
   def create
     @answer = @question.answers.new(answer_params)
@@ -9,10 +10,23 @@ class AnswersController < ApplicationController
     redirect_to question_path(@question)
   end
 
+  def destroy
+    if current_user == @answer.user
+      @answer.destroy!
+      redirect_to question_path(@answer.question), notice: 'Your answer was deleted'
+    else
+      redirect_to question_path(@answer.question), notice: "You can't do this"
+    end
+  end
+
   private
 
   def set_question
     @question = Question.find_by(id: params[:question_id])
+  end
+
+  def find_answer
+    @answer = Answer.find(params[:id])
   end
 
   def answer_params
---
+++ spec/features/answer/delete_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'User delete answer', %q{
+  probably his answer is bad
+} do
+  given(:question) { create(:question) }
+  given(:other_question) { create(:question) }
+  given(:user) { create(:user) }
+
+  describe 'Authenticated user' do
+    given!(:answer) { create(:answer, user: user, question: question) }
+    given!(:other_answer) { create(:answer, question: other_question) }
+    background { sign_in(user) }
+
+    scenario 'tries delete his answer' do
+      visit question_path(question)
+      click_on 'Delete answer'
+
+      expect(page).to have_content 'Your answer was deleted'
+    end
+
+    scenario 'tries delete other answer' do
+      visit question_path(other_question)
+
+      expect(page).to_not have_content 'Delete answer'
+    end
+  end
+
+  describe 'Authenticated user' do
+    given!(:answer) { create(:answer, user: user, question: question) }
+
+    scenario 'tries delete answer' do
+      visit question_path(question)
+
+      expect(page).to_not have_content 'Delete answer'
+    end
+  end
+end
