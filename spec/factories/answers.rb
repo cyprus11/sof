ef982c1edea600cc0742f46afe2ececc935d839c
Alexey <alexx10@mail.ru>
2022-04-12 19:47:24 +0300

    Add file when editing answer

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -11,5 +11,9 @@ FactoryBot.define do
     trait :invalid do
       body { nil }
     end
+
+    trait :with_file do
+      files {[Rack::Test::UploadedFile.new("#{Rails.root}/spec/rails_helper.rb", "text/x-ruby")]}
+    end
   end
 end
--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -5,7 +5,7 @@ feature 'User edit answer', %q{
 } do
   given(:question) { create(:question) }
   given(:user) { create(:user) }
-  given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:answer) { create(:answer, :with_file, question: question, user: user) }
   given(:other_user) { create(:user) }
 
   describe "Authenticated user", js: true do
@@ -43,6 +43,20 @@ feature 'User edit answer', %q{
       visit question_path(question)
 
       expect(page).to_not have_content 'Edit answer'
+    end
+
+    scenario 'add files to edited question' do
+      sign_in(user)
+      visit question_path(question)
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to_not have_link 'spec_helper.rb'
+
+      click_on 'Edit answer'
+      attach_file 'Answer files', ["#{Rails.root}/spec/spec_helper.rb"]
+
+      click_on 'Update Answer'
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 
