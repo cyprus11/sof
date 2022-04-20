94934c2bdb3ec6609a81efc0d4805604bd59a0a2
Alexey <alexx10@mail.ru>
2022-04-20 15:38:16 +0300

    Test methods for link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,14 @@ class Link < ApplicationRecord
 
   validates :name, :url, presence: true
   validates_with UrlValidator
+
+  def gist?
+    self.url.start_with? 'https://gist.github.com/'
+  end
+
+  def gist_id
+    return unless gist?
+
+    self.url.split('/').last
+  end
 end
