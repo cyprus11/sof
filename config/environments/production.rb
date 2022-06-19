95d9629caffc9e6dc0f961864d3db9683e3e96c4
Alexey <alexx10@mail.ru>
2022-06-19 17:35:20 +0300

    change default_url_options for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sof_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'sof.tav-dev.tk', port: 443 }
+  config.action_mailer.default_url_options = { host: 'sof.tav-dev.tk' }
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.perform_deliveries = true
 
